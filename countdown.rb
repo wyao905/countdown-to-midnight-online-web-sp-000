5c17af9aaf570fc72c25cc909c7c0951da73033a
Warren Yao <warrenyca905@gmail.com>
2019-05-17 01:36:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    sleep(1)
+    int -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
